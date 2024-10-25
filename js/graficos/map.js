62825064102a69555b6b5ed67db5a027aea5f478
Junior Facin <jufacin@hotmail.com>
2024-10-24 22:28:00 -0300

    fix: footer eliminado, alteracao de visuais e correcao do css das tabelas

--- js/graficos/map.js
+++ js/graficos/map.js
@@ -1,28 +1,24 @@
-// Cria o mapa e define o centro e o nível de zoom
 var map = L.map('map').setView([-23.55052, -46.633308], 13);
 
-// Adiciona a camada do mapa (OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-// Dados de exemplo (pode ser carregado de um arquivo ou API)
 var heatData = [
 
-    [-23.55052, -46.633308, 0.8],  // São Paulo
+    [-23.55052, -46.633308, 0.8],
 
-    [-23.55982, -46.639308, 0.7],  // Região próxima
+    [-23.55982, -46.639308, 0.7],
 
-    [-23.54932, -46.629308, 0.6],  // Região próxima
+    [-23.54932, -46.629308, 0.6],
 
-    [-23.54332, -46.633108, 0.5],  // Outra área
+    [-23.54332, -46.633108, 0.5],
 
-    [-23.55352, -46.637308, 0.9]   // Outra área
+    [-23.55352, -46.637308, 0.9]
 
 ];
 
-// Cria o mapa de calor com opções personalizadas
 L.heatLayer(heatData, {
     "radius": 20,
     "blur": 15,
--- js/relatorios.js
+++ js/relatorios.js
@@ -1,26 +1,25 @@
-// Baixar relatorio em Excel
 document.getElementById('excelBtn').addEventListener('click', function () {
-    const table = document.getElementById('transactionTable').closest('table'); // Gets the full table element
+    const table = document.getElementById('transactionTable').closest('table');
     const wb = XLSX.utils.table_to_book(table);
     XLSX.writeFile(wb, 'relatorio.xlsx');
 });
 
-// Baixar relatorio em PDF
+
 document.getElementById('pdfBtn').addEventListener('click', function () {
     const { jsPDF } = window.jspdf;
     const doc = new jsPDF();
 
-    // Pegando os dados do cabeçalho e do corpo da tabela
+
     const table = document.getElementById('transactionTable').closest('table');
     const headers = [...table.querySelectorAll('thead th')].map(th => th.innerText);
     const rows = [...table.querySelectorAll('tbody tr')].map(tr => {
         return [...tr.querySelectorAll('td')].map(td => td.innerText);
     });
 
-    // Gerando o relatório PDF com cabeçalho
+
     doc.autoTable({
-        head: [headers],   // Cabeçalhos da tabela
-        body: rows,        // Dados do corpo da tabela
+        head: [headers],
+        body: rows,
     });
 
     doc.save('relatorio.pdf');
